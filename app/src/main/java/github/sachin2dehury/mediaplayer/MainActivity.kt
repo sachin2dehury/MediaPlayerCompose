5e219cff70cdd8978e03d6bc3ef609a0a54aff98
Sachin Dehury <51115041+sachin2dehury@users.noreply.github.com>
2023-07-01 14:10:48 +0530

    First commit

---
+++ app/src/main/java/github/sachin2dehury/mediaplayer/MainActivity.kt
@@ -0,0 +1,25 @@
+package github.sachin2dehury.mediaplayer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import github.sachin2dehury.mediaplayer.ui.theme.MediaPlayerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MediaPlayerTheme {
+                // A surface container using the 'background' color from the theme
+
+            }
+        }
+    }
+}
